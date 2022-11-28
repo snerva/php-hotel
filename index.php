4c892656bcf553485230c30ea3bb62b5adeae5d2
snerva <selenenerva@live.it>
2022-11-28 15:37:38 +0100

    add bootstrap

--- index.php
+++ index.php
@@ -50,18 +50,36 @@ var_dump($hotels);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 
 <body>
-    <?php foreach ($hotels as $hotel) : ?>
-        <ul>
-            <li> <?php echo $hotel['name'] ?> </li>
-            <li> <?php echo $hotel['description'] ?> </li>
-            <li> <?php echo $hotel['parking'] ?> </li>
-            <li> <?php echo $hotel['vote'] ?> </li>
-            <li> <?php echo $hotel['distance_to_center'] ?> </li>
-        </ul>
-    <?php endforeach; ?>
+    <div class="container">
+        <h1 class="text-center">Hotels</h1>
+        <table class="table">
+            <thead>
+                <tr class="table-primary">
+                    <th scope="col">Name</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Parking</th>
+                    <th scope="col">Vote</th>
+                    <th scope="col">Distance to center</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach ($hotels as $hotel) : ?>
+                    <tr>
+                        <th scope="row"> <?php echo $hotel['name'] ?> </th>
+                        <td> <?php echo $hotel['description'] ?> </td>
+                        <td> <?php echo $hotel['parking'] ?> </td>
+                        <td> <?php echo $hotel['vote'] ?> </td>
+                        <td> <?php echo $hotel['distance_to_center'] ?> </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+
 </body>
 
 </html>
